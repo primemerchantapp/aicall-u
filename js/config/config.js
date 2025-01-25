571196a11ff3e6decb7af12adc4f13026ab581c9
masterdee121212 <primemerchantapp@gmail.com>
2025-01-26 05:00:28 +0800

    Update config.js

--- js/config/config.js
+++ js/config/config.js
@@ -13,7 +13,7 @@ export const CONFIG = {
     },
     AUDIO: {
         INPUT_SAMPLE_RATE: 16000,
-        OUTPUT_SAMPLE_RATE: 24000, // Adjust this to change pitch as desired
+        OUTPUT_SAMPLE_RATE: 22000, // Adjust this to change pitch as desired
         BUFFER_SIZE: 7680,
         CHANNELS: 1,
     },
